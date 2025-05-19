d2ae6b707c533fa094708f578e3928eed5af9b12
Christopher Paez <paez.christopher@gmail.com>
2025-05-19 19:41:23 -0400

    Finish Secret Tunnel Workshop

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import axios from "axios";
 
 const API = "https://fsa-jwt-practice.herokuapp.com";
 
@@ -9,10 +10,44 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  const signup = async (username) => {
+    try {
+      const response = await axios.post(`${API}/signup`, {
+        username,
+        password: "super-secret-999",
+      });
+      console.log(response.data);
+      if (response.data.success) {
+        setToken(response.data.token);
+        setLocation("TABLET");
+      } else {
+        throw new Error("Could not validate user");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   // TODO: authenticate
+  const authenticate = async () => {
+    if (!token) {
+      throw new Error("Error: No token ");
+    }
+    try {
+      const res = await axios(`${API}/authenticate`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (res.data.success) {
+        setLocation("TUNNEL");
+      } else {
+        throw new Error("Error: Cannot validate user");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  const value = { location };
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
